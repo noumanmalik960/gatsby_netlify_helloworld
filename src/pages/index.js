b6af38ed072b2be66b25c6715d20038c9993e673
noumanmalik960 <noumanmalik960@gmail.com>
2021-01-20 01:45:49 +0500

    updated index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from './index.module.css'
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.top_heading}>Hello world from Nouman!</h1>
+      <h1 className={styles.top_heading}>Hello world from Nawaz Shareef!</h1>
       <p className={styles.paragraph}>lorem ipsum dolor sit amet!!!!</p>
     </div>
   )
