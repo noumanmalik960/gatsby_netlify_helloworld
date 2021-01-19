fe6c3900587da37c304e223facbfe2e7aeb6f84d
noumanmalik960 <noumanmalik960@gmail.com>
2021-01-20 01:41:50 +0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import styles from './index.module.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1 className={styles.top_heading}>Hello world from Nouman!</h1>
+      <p className={styles.paragraph}>lorem ipsum dolor sit amet!!!!</p>
+    </div>
+  )
 }
